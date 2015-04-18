c83616c4b698c87c0454814c0f7fd09237c0aebd
Abbass-Ali Siwjee <abu.siwjee@utoronto.ca>
2015-04-18 11:39:41 -0400

    To Do List Finished
    To Do list works completely now! Does not utilize descriptions though!

--- iOS Course/5. Content, Tables/To Do List/To Do List/FirstViewController.swift
+++ iOS Course/5. Content, Tables/To Do List/To Do List/FirstViewController.swift
@@ -8,16 +8,53 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var toDoList = [toDoItem].self
+var cellContent = [String]()
 
+class FirstViewController: UIViewController, UITableViewDelegate {
+    
+    @IBOutlet weak var toDoListTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //check if the key we want exists, if so, read it in to the cell content list we have with the key todoList (perm storage)
+        if NSUserDefaults.standardUserDefaults().objectForKey("toDoList") != nil {
+            cellContent = NSUserDefaults.standardUserDefaults().objectForKey("toDoList") as! [String]
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return cellContent.count
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = cellContent[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        cellContent.removeAtIndex(indexPath.row)
+        NSUserDefaults.standardUserDefaults().setObject(cellContent, forKey: "toDoList")
+        toDoListTable.reloadData()
+        
+    }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        toDoListTable.reloadData()
     }
 
 
--- iOS Course/5. Content, Tables/To Do List/To Do List/SecondViewController.swift
+++ iOS Course/5. Content, Tables/To Do List/To Do List/SecondViewController.swift
@@ -7,17 +7,54 @@
 //
 
 import UIKit
+    struct toDoItem {
+        var name: String
+        var desc: String
+    }
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var nameOfItem: UITextField!
+    @IBOutlet weak var descOfItem: UITextField!
+    @IBOutlet weak var outputMessage: UILabel!
+    
+    
+
+
+    @IBAction func addItems(sender: AnyObject) {
+        let newItem = toDoItem(name: nameOfItem.text, desc: descOfItem.text)
+        cellContent.append(nameOfItem.text)
+        nameOfItem.text = ""
+        descOfItem.text = ""
+        
+        //Save permenantly the list cellcontent with key todolist
+        NSUserDefaults.standardUserDefaults().setObject(cellContent, forKey: "toDoList")
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //need these 2 lines for the keyboard going away as well
+        self.nameOfItem.delegate = self
+        self.descOfItem.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    
+    //these next 2 functions are for keyboard, making it go away with enter or return
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        self.view.endEditing(true)
     }
 
 
